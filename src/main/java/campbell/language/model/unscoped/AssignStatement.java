f43515e837d9e360949217f6f7564724d1aa5230
Sophie Lathouwers <s.a.m.lathouwers@student.utwente.nl>
2015-06-30 10:57:32 +0200

    Added documentation to first three classes of unscoped Campbell

--- src/main/java/campbell/language/model/unscoped/AssignStatement.java
+++ src/main/java/campbell/language/model/unscoped/AssignStatement.java
@@ -9,7 +9,13 @@ import campbell.roborovski.model.Block;
 import campbell.roborovski.model.Program;
 
 public class AssignStatement extends Statement {
+    /**
+     * Expression representing left side of the assignment
+     */
     private final Expression left;
+    /**
+     * Expression representing right side of the assignment
+     */
     private final Expression right;
 
     public AssignStatement(Expression left, Expression right) {
@@ -22,6 +28,10 @@ public class AssignStatement extends Statement {
         return at(assign.getStart(), new AssignStatement(Expression.fromContext(assign.expr(0)), Expression.fromContext(assign.expr(1))));
     }
 
+    /**
+     * Sets the scope of this assignment and its expressions
+     * @param scope
+     */
     @Override
     public void setScope(Scope scope) {
         this.scope = scope;
@@ -30,21 +40,41 @@ public class AssignStatement extends Statement {
         right.setScope(scope);
     }
 
+    /**
+     * Makes a string representation of this assignment with correct indenting
+     * @param indent - indent level of this assignment
+     * @return string representation of this assignment
+     */
     @Override
     public String toString(int indent) {
         return left.toString(indent) + " = " + right.toString(0);
     }
 
+    /**
+     * Converts this assignment to the IR Roborovski
+     *
+     * @param program
+     * @param block
+     */
     @Override
     public void toRoborovski(Program program, Block block) {
         block.addStatement(new Assign(left.toRoborovski(program), right.toRoborovski(program)));
     }
 
+    /**
+     * Makes a deep copy of this assignment
+     * @return deep copy of this assignment
+     */
     @Override
     public AssignStatement deepCopy() {
         return new AssignStatement(left.deepCopy(), right.deepCopy());
     }
 
+    /**
+     * Replaces a given type by another given type within this assignment
+     * @param replace - type that should be replaced
+     * @param replaceWith - replacement type
+     */
     @Override
     public void replaceType(Type replace, Type replaceWith) {
         left.replaceType(replace, replaceWith);
